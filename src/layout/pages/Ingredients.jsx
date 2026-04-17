2d5399f6ccdab0fd189eecbacb571102818dbed7
asonkes <audrey.sonkes@gmail.com>
2026-04-17 10:58:57 +0200

    composant ingredient en formation

--- src/layout/pages/Ingredients.jsx
+++ src/layout/pages/Ingredients.jsx
@@ -36,21 +36,33 @@ export const Ingredients = () => {
         <ul className="grid grid-cols-4 gap-8">
           {ingredients.map((ingredient) => (
             <li
-              className="odd:bg_ingredient1 even:bg_ingredient2 bg-no-repeat bg-cover bg-center"
+              className="relative odd:bg_ingredient1 even:bg_ingredient2 bg-no-repeat bg-cover bg-center z-1"
               key={ingredient._id}
             >
-              <h3 className="font-montserrat text-primary-700 text-lg text-center p-3">
+              <h3 className="font-montserrat font-semibold text-primary-600 text-lg text-center p-3">
                 {ingredient.name}
               </h3>
-              <div className="px-12 border-4 border-red-600">
+              <div className="px-8 border-4 border-red-600 z-3">
                 <img
-                  className="rounded-t-[9.5rem] border-4 border-blue-800"
+                  className="w-[13.75rem] h-[17.5rem] object-cover rounded-t-[9.5rem] m-auto border-4 border-blue-800"
                   src={`/images/ingredients/${ingredient.slug}.webp`}
                   alt={`Image représentant l'ingrédient '${ingredient.name}' sur fond en bois foncé`}
                 />
+                <div className="font-bree_Serif border-4 border-green-500 py-4">
+                  {ingredient.description}
+                </div>
                 <div className="flex justify-around py-2 border-4 border-amber-300">
-                  <Button className="p-3.5" text="Ajouter" />
-                  <Button className="p-3.5" text="Voir plus" />
+                  <button className="font-montserrat font-semibold bg-primary-600 text-white cursor-pointer py-2 px-4 hover:scale-105 transition-transorm duration-300 ease-in-out">
+                    Ajouter
+                  </button>
+                  <button className="font-montserrat font-semibold bg-primary-600 text-white cursor-pointer py-2 px-4 hover:scale-105">
+                    Voir plus
+                  </button>
+                </div>
+              </div>
+              <div className="absolute top-[50%] h-[50%] border-4 border-blue-500 py-2 px-6 -z-2">
+                <div className="bg-white border-4 border-red-400 h-full">
+                  {ingredient.description}
                 </div>
               </div>
             </li>
