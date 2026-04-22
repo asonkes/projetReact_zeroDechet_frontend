fcf61708b73661afdc9adc57c79f5e8e6be812e0
asonkes <audrey.sonkes@gmail.com>
2026-04-22 10:54:06 +0200

    refactorisation de 'About' pour le rendre + clair pour l'utilisateur

--- src/layout/pages/About.jsx
+++ src/layout/pages/About.jsx
@@ -1,20 +1,20 @@
 import { FullScreen } from "../../shared/FullScreen";
 import { Title } from "../../shared/Title";
+import { Button } from "../../shared/button/Button";
 
 export const About = (props) => {
-
-  const {id} = props;
+  const { id } = props;
 
   return (
-    <section id={id} className="w-fullflex">
+    <section id={id} className="w-full">
       <FullScreen
         height="min-h-[calc(100vh-106.5px)]"
-        className="flex-col bg_about_mobile lg:bg_about bg-cover bg-center"
+        className="flex flex-col bg_about_mobile lg:bg_about bg-cover bg-center"
       >
-        <div className="container flex flex-col">
+        <div className="container flex flex-col border-4 border-green-400">
           <Title text="About" />
 
-          <div className="flex flex-col font-borel font-bold text-xl lg:text-2xl text-center mt-13 px-4 text-tertiary-500">
+          <div className="flex flex-col font-borel font-bold text-lg lg:text-xl text-center mt-5 px-4 text-tertiary-500">
             <p className="py-2">
               Quel est notre objectif ? Vous avez des légumes dans votre
               potager?
@@ -22,7 +22,7 @@ export const About = (props) => {
             <p className="py-2">En 2 clicks, vous trouvez votre recette...</p>
           </div>
 
-          <div className="flex flex-col font-bree_Serif text-lg sm:text-xl text-center italic mt-13 mb-8 px-4">
+          <div className="flex flex-col font-bree_Serif text-base lg:text-lg text-center italic mt-5 px-4">
             <p className="py-2">
               <span className="underline underline-offset-4">
                 L’idée est simple :
@@ -55,6 +55,25 @@ export const About = (props) => {
               Une cuisine écologique, créative et pensée pour le quotidien.
             </p>
           </div>
+
+          <div className="font-bree_Serif text-center mt-5">
+            <h4 className="text-lg lg:text-xl py-2">
+              <span className="underline underline-offset-4">
+                Le principe est simple :
+              </span>
+              <span>☺️</span>
+            </h4>
+            <div className="text-base lg:text-lg">
+              <p className="py-2">🩷 1. Je choisis mes ingrédients</p>
+              <p className="py-2">🩷 2. Je découvre mes recettes</p>
+              <p className="py-2">🩷 3. Je cuisine sans gaspiller</p>
+            </div>
+          </div>
+          <Button
+            to="/ingredients"
+            text="Je sélectionne mes ingrédients"
+            className="mt-2 mb-6 mx-auto sm:mt-8"
+          />
         </div>
       </FullScreen>
     </section>
