1a9da68872c78f91e97c9b592cc6a980f125d3c9
asonkes <audrey.sonkes@gmail.com>
2026-04-29 15:47:31 +0200

    menu burger ok - prob police à voir

--- src/layout/components/Header.jsx
+++ src/layout/components/Header.jsx
@@ -1,15 +1,52 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { NavItem } from "../../shared/NavItem";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faCircleUser } from "@fortawesome/free-regular-svg-icons";
 
 export const Header = () => {
   const [isActive, setIsActive] = useState(false);
+  const menuRef = useRef();
+
+  /* On utilise un useEffect pour agir en dehors du composant */
+  useEffect(() => {
+    const closeBurger = (e) => {
+      /* Et on vérifie si le click a eu lieu en dehors ou dans le menu */
+      if (!menuRef.current.contains(e.target)) {
+        setIsActive(false);
+      }
+    };
+
+    document.addEventListener("click", closeBurger);
+
+    /* On enlève l'écouteur d'évènement */
+    return () => removeEventListener("click", closeBurger);
+  }, []);
+
+  /* 'isActive' dans le tableau des dépendances => car on veut qu'il se déclenche à chaque fois que 'isActive' change */
+  useEffect(() => {
+    if (isActive) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+
+    /* Evite de retenir l'évènement */
+    /* Ex : j'ai ouvert mon menu => scroll annulé
+    Je vais sur une autre page ==> annulé je peux scroller */
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, [isActive]);
+
   return (
-    <header className="w-full min-h-3-75 border border-primary-600 shadow-primary-400 shadow fixed bg-white z-50">
-      <nav className="relative w-full min-h-3-75 inline-flex justify-center">
-        <ul className="max-w-full absolute right-0 top-[66px] lg:top-0 lg:relative flex flex-col lg:flex-row p-1 bg-white border border-primary-600 shadow-primary-400 lg:border-none lg:shadow-none">
+    <header className="w-full min-h-3-75 flex items-center border border-primary-600 shadow-primary-400 shadow fixed bg-white z-50">
+      <nav className="relative w-full h-3-75 inline-flex justify-center">
+        <ul
+          ref={menuRef}
+          className={`max-w-full h-[calc(100vh-60px)] absolute right-0 top-3-75 flex flex-col p-1 bg-white border border-primary-600 shadow-primary-400 shadow lg:relative lg:h-auto lg:top-0 lg:flex-row lg:border-none lg:shadow-none transition-opacity duration-300 ease-in ${isActive ? `opacity-100` : `opacity-0 lg:opacity-100`}`}
+        >
           <NavItem to="/" text="Home" />
           <NavItem to="/#about" text="About" />
           <NavItem
@@ -19,9 +56,15 @@ export const Header = () => {
           />
           <NavItem to="/recipes" text="Recettes" />
           <NavItem to="/contact" text="Contact" />
+          <NavItem to="/login">
+            <FontAwesomeIcon icon={faCircleUser} />
+          </NavItem>
         </ul>
         <div
-          onClick={() => setIsActive(!isActive)}
+          onClick={(e) => {
+            e.stopPropagation();
+            setIsActive(!isActive);
+          }}
           className="max-w-full h-full absolute top-0 right-0 flex items-center mr-4 lg:hidden cursor-pointer"
         >
           {!isActive ? (
--- src/shared/NavItem.jsx
+++ src/shared/NavItem.jsx
@@ -1,7 +1,7 @@
 import { NavLink, useLocation } from "react-router-dom";
 import { NavHashLink } from "react-router-hash-link";
 
-export const NavItem = ({ text, to, className = "" }) => {
+export const NavItem = ({ children, text, to, className = "" }) => {
   const location = useLocation();
   const isHash = to.includes("#");
   const Component = isHash ? NavHashLink : NavLink;
@@ -14,7 +14,7 @@ export const NavItem = ({ text, to, className = "" }) => {
 
   return (
     <li
-      className={`p-3 mx-2 font-borel text-xl hover:scale-105 transition-transform duration-200 ease-out ${className}`}
+      className={`border-b-primary-600 px-3 py-4 mx-2 font-borel text-xl hover:scale-105 transition-transform duration-200 ease-out border-b-2 lg:flex lg:items-center lg:py-0 lg:border-none ${className}`}
     >
       <Component
         to={to}
@@ -30,7 +30,7 @@ export const NavItem = ({ text, to, className = "" }) => {
             return `${
               location.pathname === "/" && location.hash === ""
                 ? "text-secondary-500"
-                : "text-primary-600"
+                : "text-primary-600 flex items-center hover:text-secondary-500"
             }`;
           }
 
@@ -39,15 +39,15 @@ export const NavItem = ({ text, to, className = "" }) => {
             return `${
               location.hash === "#about"
                 ? "text-secondary-500"
-                : "text-primary-600"
+                : "text-primary-600 flex items-center hover:text-secondary-500"
             }`;
           }
 
           /* Pour le reste */
-          return `${isActive ? "text-secondary-500" : "text-primary-600"}`;
+          return `${isActive ? "text-secondary-500" : "text-primary-600 flex items-center hover:text-secondary-500"}`;
         }}
       >
-        {text}
+        {children || text}
       </Component>
     </li>
   );
