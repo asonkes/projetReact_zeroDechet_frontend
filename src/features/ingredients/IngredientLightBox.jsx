da18cc6bc3e4aaf890fa130fb2cdca0976ecc4df
Audrey Sonkes <a.sonkes@interface3.be>
2026-05-13 13:18:05 +0200

    modification css click image ingredient + mise en place d'un favicon

--- src/features/ingredients/IngredientLightBox.jsx
+++ src/features/ingredients/IngredientLightBox.jsx
@@ -5,7 +5,7 @@ import { faCircleXmark } from "@fortawesome/free-regular-svg-icons";
 export const IngredientLightBox = (props) => {
   const { src, alt, onClose, className = "" } = props;
   return (
-    <div className="w-full h-[calc(100vh-102px)] absolute top-3-75 right-0 justify-center bg-black px-16 z-40">
+    <div className="w-full h-[calc(100vh-102px)] absolute top-3-75 right-0 justify-center bg-black px-16 z-40 overflow-hidden overscroll-y-none">
       <div className="relative h-full flex justify-center items-center p-0 md:p-16">
         <img
           className={`m-auto cursor-pointer border-4 border-white ${className}`}
